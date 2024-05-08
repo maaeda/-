ca46e754310fb6913f960264a1a108463c28030e
Torxscrews <zeal23410074@fsgcl.jp>
2024-05-08 19:24:54 +0900

    ビルドエラー修正

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timelabel = new System.Windows.Forms.Label();
+            this.foodlabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(487, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timelabel
+            // 
+            this.timelabel.AutoSize = true;
+            this.timelabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.timelabel.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.timelabel.Location = new System.Drawing.Point(168, 26);
+            this.timelabel.Name = "timelabel";
+            this.timelabel.Size = new System.Drawing.Size(135, 48);
+            this.timelabel.TabIndex = 1;
+            this.timelabel.Text = "label1";
+            // 
+            // foodlabel
+            // 
+            this.foodlabel.AutoSize = true;
+            this.foodlabel.Location = new System.Drawing.Point(209, 84);
+            this.foodlabel.Name = "foodlabel";
+            this.foodlabel.Size = new System.Drawing.Size(35, 12);
+            this.foodlabel.TabIndex = 1;
+            this.foodlabel.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.foodlabel);
+            this.Controls.Add(this.timelabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label timelabel;
+        private System.Windows.Forms.Label foodlabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*画像の表示*/
+            this.Text = ProductName;
+            // ピクチャボックスの設定
+            //pictureBox1.Dock = DockStyle.Fill;    // フォームいっぱいに貼り付ける
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // 縦横比を変えずに引き延ばす
+            pictureBox1.Image = Properties.Resources.test;
+            /************/
+
+            timer1.Start();
+            foodlabel.Text = " ";
+
+
+        }
+
+        private void timer1_tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            timelabel.Text = now.ToLongTimeString();
+        }
+    }
+}
