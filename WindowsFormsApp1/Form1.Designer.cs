9e235c1da1e72bea7e5629e84e06289c8bd6af4d
riku0817 <inbederacade@gmail.com>
2024-05-10 16:19:03 +0900

    APIに必要なNewtonsoft.JsonとSystem.Net.Httpの実装
    先生の提示したAPIに必要なファイルを入れときました

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -34,15 +34,17 @@
             this.timelabel = new System.Windows.Forms.Label();
             this.foodlabel = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.morninglabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(769, 67);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.pictureBox1.Location = new System.Drawing.Point(473, 42);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(463, 240);
+            this.pictureBox1.Size = new System.Drawing.Size(285, 150);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -51,10 +53,10 @@
             this.timelabel.AutoSize = true;
             this.timelabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.timelabel.Font = new System.Drawing.Font("Meiryo UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.timelabel.Location = new System.Drawing.Point(201, 67);
-            this.timelabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.timelabel.Location = new System.Drawing.Point(124, 42);
+            this.timelabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.timelabel.Name = "timelabel";
-            this.timelabel.Size = new System.Drawing.Size(329, 122);
+            this.timelabel.Size = new System.Drawing.Size(207, 77);
             this.timelabel.TabIndex = 1;
             this.timelabel.Text = "label1";
             // 
@@ -62,10 +64,10 @@
             // 
             this.foodlabel.AutoSize = true;
             this.foodlabel.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.foodlabel.Location = new System.Drawing.Point(287, 246);
-            this.foodlabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.foodlabel.Location = new System.Drawing.Point(177, 154);
+            this.foodlabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.foodlabel.Name = "foodlabel";
-            this.foodlabel.Size = new System.Drawing.Size(83, 30);
+            this.foodlabel.Size = new System.Drawing.Size(53, 19);
             this.foodlabel.TabIndex = 1;
             this.foodlabel.Text = "label1";
             // 
@@ -73,15 +75,36 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_tick);
             // 
+            // morninglabel
+            // 
+            this.morninglabel.AutoSize = true;
+            this.morninglabel.Location = new System.Drawing.Point(481, 246);
+            this.morninglabel.Name = "morninglabel";
+            this.morninglabel.Size = new System.Drawing.Size(72, 15);
+            this.morninglabel.TabIndex = 2;
+            this.morninglabel.Text = "foods text";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(473, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "朝の食事一覧";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1377, 900);
+            this.ClientSize = new System.Drawing.Size(847, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.morninglabel);
             this.Controls.Add(this.foodlabel);
             this.Controls.Add(this.timelabel);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -97,5 +120,7 @@
         private System.Windows.Forms.Label timelabel;
         private System.Windows.Forms.Label foodlabel;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label morninglabel;
+        private System.Windows.Forms.Button button1;
     }
 }
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,15 +10,23 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+
     public partial class Form1 : Form
     {
+        string[] breakfastfoods = { "aaa", "bbb", "ccc" };// 朝ご飯を入れる大域変数
+        string[] lunchfoods = { "aaa", "bbb", "ccc" };// 朝ご飯を入れる大域変数
+        string[] dinnerfoods = { "aaa", "bbb", "ccc" };// 朝ご飯を入れる大域変数
+
+
         public Form1()
         {
+            
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             /*画像の表示*/
             this.Text = ProductName;
             //pictureBox1.Dock = DockStyle.Fill;    // フォームいっぱいに貼り付ける
@@ -37,6 +45,12 @@ namespace WindowsFormsApp1
             timelabel.Text = now.ToLongTimeString();
         }
 
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(string morning in breakfastfoods) //foreach文で指定した配列全部を参照する
+            {
+                morninglabel.Text = morning;
+            }
+        }
     }
 }
