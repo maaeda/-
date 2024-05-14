616b3f881f317d22acd474582b248d4d4ebaec54
Maeda jo <zeal23410074@fsgcl.jp>
2024-05-15 03:52:09 +0900

    i don't know

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,6 +11,7 @@ using static System.Net.WebRequestMethods;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace WindowsFormsApp1
 {
@@ -21,31 +22,23 @@ namespace WindowsFormsApp1
         string[] lunchfoods = { "aaa", "bbb", "ccc" };// 朝ご飯を入れる大域変数
         string[] dinnerfoods = { "aaa", "bbb", "ccc" };// 朝ご飯を入れる大域変数
 
-
         public Form1()
         {
-            
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
             /*画像の表示*/
             this.Text = ProductName;
-            //pictureBox1.Dock = DockStyle.Fill;    // フォームいっぱいに貼り付ける
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // 縦横比を変えずに引き延ばす
             pictureBox1.Image = Properties.Resources.test; //画像表示
             /************/
-
             /*API*/
-            string apiUrl = "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1062554798332159397";
-
+            string foodApiUrl = "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1062554798332159397";
             /*****/
 
             timer1.Start();
-            foodlabel.Text = " ";
-
         }
 
         private void timer1_tick(object sender, EventArgs e)
@@ -56,8 +49,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            foodlabel.Text = "HELP!!!!!!!!!!!!!!!!";
-                
+
         }
     }
 }
