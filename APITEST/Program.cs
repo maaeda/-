7b9a1c3069b658a385283b3468ab251f1087b466
riku0817 <inbederacade@gmail.com>
2024-05-14 15:48:14 +0900

    new hope

--- APITEST/Program.cs
+++ APITEST/Program.cs
@@ -1,30 +1,30 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
-
-class Program
+namespace WeatherConsoleApp
 {
-    static async Task Main(string[] args)
+    class Program
     {
-        var url = $"https://weather.tsukumijima.net/api/forecast/city/400040";
-
-        using (var client = new HttpClient())
+        static async Task Main(string[] args)
         {
-            var response = await client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(content);
-            }
-            else
-            {
-                Console.WriteLine($"Error: {response.StatusCode}");
-            }
+            string apiUrl = "https://weather.tsukumijima.net/api/forecast/city/070010";
+            await DisplayWeather(apiUrl);
         }
 
+        static async Task DisplayWeather(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(url);
+                var weatherData = JObject.Parse(response);
+
+                Console.WriteLine("地域: " + weatherData["location"]["district"]);
+                Console.WriteLine("天気: " + weatherData["forecasts"][0]["telop"]);
+                Console.WriteLine("最高気温: " + weatherData["forecasts"][0]["temperature"]["max"]["celsius"] + "℃");
+                Console.WriteLine("最低気温: " + weatherData["forecasts"][0]["temperature"]["min"]["celsius"] + "℃");
+            }
+        }
     }
 }
-
