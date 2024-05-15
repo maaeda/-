325deeac1dbc4afc088b7e8bc6f94dfcc17fed81
riku0817 <inbederacade@gmail.com>
2024-05-15 14:39:02 +0900

    ボタンを押すとfoodlabelの所に天気を表示する仕組みを追加（これを主軸に料理や温度を判別するようにしたい）

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,14 +9,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Net.WebRequestMethods;
 using Newtonsoft.Json;
-using System.Collections.Generic;
 using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
 
 namespace WindowsFormsApp1
 {
 
     public partial class Form1 : Form
     {
+
+        private readonly HttpClient _httpClient = new HttpClient();
+
         string[] breakfastfoods = { "aaa", "bbb", "ccc" };// 朝ご飯を入れる大域変数
         string[] lunchfoods = { "aaa", "bbb", "ccc" };// 朝ご飯を入れる大域変数
         string[] dinnerfoods = { "aaa", "bbb", "ccc" };// 朝ご飯を入れる大域変数
@@ -54,10 +58,51 @@ namespace WindowsFormsApp1
             timelabel.Text = now.ToLongTimeString();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            foodlabel.Text = "HELP!!!!!!!!!!!!!!!!";
-                
+
+            string apiUrl = "https://weather.tsukumijima.net/api/forecast/city/070010";
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // JSONデータを解析します。
+                JObject weatherData = JObject.Parse(responseBody);
+
+                // 「天気」の項目を取得します。
+                string weather = (string)weatherData["forecasts"][0]["telop"];
+
+                // 天気に応じた画像を表示します。
+                // 例えば、「晴れ」の場合は晴れのラベルを、
+                // 「雨」の場合は雨のラベルを表示します。
+                // あとは自由に天気を追加することも可( 雨のち晴れ など)
+                switch (weather)
+                {
+                    case "晴れ":
+                        foodlabel.Text = "晴れ";
+                        break;
+                    case "曇り":
+                        foodlabel.Text = "曇り";
+                        break;
+                    case "雨":
+                        foodlabel.Text = "雨";
+                        break;
+                    case "晴のち曇":
+                        foodlabel.Text = "晴れのち曇り";
+                        break;
+                    // 他の天気の条件も同様に追加します。
+                    default:
+                        foodlabel.Text = "不明";
+                        break;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"エラーが発生しました: {ex.Message}");
+            }
+
         }
     }
 }
